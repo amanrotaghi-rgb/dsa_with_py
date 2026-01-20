964ce53fd99fa437e7d63cfdfcb2654963ec4c4f
amanDataForge <amanrotaghi@gmail.com>
2026-01-20 18:28:19 +0300

    your message

---
+++ 01_Arrays/removedublicate.py
@@ -0,0 +1,11 @@
+array = [1,2,3,2,3,5,6,3,4,2,4]
+seen = set()
+result = []
+for x in array:
+    if x not in seen:
+        result.append(x)
+        seen.add(x)
+        
+
+print(result)
+             
