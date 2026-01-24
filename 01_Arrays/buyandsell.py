447afea090ca1597dd8479b38e47e5355af4f4d7
amanDataForge <amanrotaghi@gmail.com>
2026-01-24 10:46:45 +0300

    your message

---
+++ 01_Arrays/buyandsell.py
@@ -0,0 +1,11 @@
+prices = [7, 1, 5, 3, 6, 4]
+
+min_price = prices[0]
+max_profit = 0
+
+for price in prices:
+    profit = price - min_price
+    max_profit = max(max_profit, profit)
+    min_price = min(min_price, price)
+
+print(max_profit)
