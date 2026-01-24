a176f364773ad1a85567d76afe784095f903cfe0
amanDataForge <amanrotaghi@gmail.com>
2026-01-24 10:36:34 +0300

    your message

---
+++ 01_Arrays/mostappearelement.py
@@ -0,0 +1,13 @@
+arr = [1,2,2,2,1,2,1,2,2,2]
+candidate = None
+count = 0
+for x in arr:
+    if count == 0:
+        candidate = x
+        count = 1 
+    elif x == x
+ candidate:
+        count += 1
+    elif x != candidate:
+        count -= 1
+print(candidate)        
